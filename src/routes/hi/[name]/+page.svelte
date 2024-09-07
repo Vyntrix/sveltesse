8578a9e9d8a0a507927392b0e79e9a01abc247fe
Vyntrix <frisch_valentin@icloud.com>
2024-09-07 10:37:15 +0200

    fix: prerender for static site

--- src/routes/hi/[name]/+page.svelte
+++ src/routes/hi/[name]/+page.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = false;
+</script>
+
 <script lang="ts">
 	import LL from '$lib/i18n/i18n-svelte'
 
---
+++ src/routes/hi/[name]/+page.ts
@@ -0,0 +1,7 @@
+export const load = ({ params }) => {
+  const { name } = params;
+
+  return {
+    name
+  };
+};
